9d4ba03c9e81b4f1244b5d50b25e2d03995fa5df
Subha Mishra <80328636+SubhaMishra1998@users.noreply.github.com>
2023-04-26 19:36:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,6 +6,6 @@ resource "aws_instance" "ec2-user" {
   ami           = "ami-02eb7a4783e7e9317" # us-west-2
   instance_type = "t2.micro"
   tags = {
-      Name = "server 2"
+      Name = "infra-server"
   }
 }
